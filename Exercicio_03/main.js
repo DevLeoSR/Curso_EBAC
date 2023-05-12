886da8d4e8b01af27ba07afda1fefd61dcf894fe
Leonardo de Souza Rezende <devleozaosr@gmail.com>
2023-05-12 19:27:33 -0300

    exercicio do modulo

--- Exercicio_03/main.js
+++ Exercicio_03/main.js
@@ -2,4 +2,24 @@ $(document).ready(function(){
   $('#iptTel').mask('(00)00000-0000');
   $('#iptCep').mask('00000-000');
   $('#iptCpf').mask('000.000.000-00');
+
+
+  $('form').on('submit', () => {
+    let nome = $('#iptNome').val()
+    let tel = $('#iptTel').val()
+    let email = $('#iptEmail').val()
+    let Cep = $('#iptCep').val()
+    let endereco = $('#iptEndereco').val()
+    let Cpf = $('#iptCpf').val()
+
+    alert(
+      `Cliente: ${nome} \n` +
+      `telefone: ${tel}\n` +
+      `E-mail: ${email}\n` +
+      `CEP: ${Cep}\n` +
+      `CEP: ${endereco}\n` +
+      `CPF: ${Cpf}\n\n` +
+      'Cadastrado com sucesso!')
+
+  })
 });
